53da9078d0d10aeebbfd91489e63e0c4695a9915
Ranjith Nori <ranjithnori@gmail.com>
2020-11-18 16:56:21 +0530

    spec1 added

---
+++ cypress/integration/spec1.js
@@ -0,0 +1,20 @@
+describe('Block 1', () => {
+  beforeEach(() => {
+    window.localStorage.setItem('secret', 'random');
+    cy.route2('https://staging.api.kisi.io/user').as('getBaseUser');
+  });
+
+  it('Silly test 1', () => {
+    cy.visit('https://staging.web.kisi.io/users/sign_up');
+  });
+});
+
+describe('Block 2', () => {
+  beforeEach(() => {console.error("before Each")});
+
+  it('Silly test 2', () => {
+    cy.visit('https://staging.web.kisi.io/users/sign_in');
+    cy.contains('Random text');
+  });
+});
+
